ccaf5529dc05c70f69a7da7c65a543bf3cca7f9e
bjennings90 <101131665+bjennings90@users.noreply.github.com>
2022-10-14 08:42:43 -0500

    added users, friends, thoughts, reactions

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,40 +1,51 @@
-// import the gql tagged template function
 const { gql } = require('apollo-server-express');
 
-//create our typeDefs
 const typeDefs = gql`
-
-type User {
+  type User {
     _id: ID
     username: String
     email: String
     friendCount: Int
     thoughts: [Thought]
     friends: [User]
-}
+  }
 
-type Thought {
+  type Thought {
     _id: ID
     thoughtText: String
     createdAt: String
     username: String
     reactionCount: Int
     reactions: [Reaction]
-}
+  }
 
-type Reaction {
+  type Reaction {
     _id: ID
     reactionBody: String
     createdAt: String
     username: String
-}
+  }
 
-type Query {
+  type Query {
+    me: User
     users: [User]
     user(username: String!): User
-  thoughts(username: String): [Thought]
-  thought(_id: ID!): Thought
-}`
+    thoughts(username: String): [Thought]
+    thought(_id: ID!): Thought
+  }
 
-//export the typeDefs
+  type Mutation {
+    login(email: String!, password: String!): User
+    addUser(username: String!, email: String!, password: String!): User
+    addThought(thoughtText: String!): Thought
+    addReaction(thoughtId: ID!, reactionBody: String!): Thought
+    addFriend(friendId: ID!): User
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+`;
+
 module.exports = typeDefs;
--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+const { authMiddleware } = require('./utils/auth');
+
 const express = require('express');
 //import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
@@ -10,7 +12,8 @@ const PORT = process.env.PORT || 3001;
 //create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware
 });
 const app = express();
 
